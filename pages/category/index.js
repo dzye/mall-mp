cd41c71f4279c746ac4c9e76555fecb0cea55ddc
dzye <756611752@qq.com>
2023-12-01 00:35:03 +0800

    feat: 分类页面实现

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,40 @@
-// pages/category/index.js
+import {
+  request,
+  baseUrl
+} from "../../util/request"
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    selectId: 1,
+    baseURL: '',
+    leftMenuList: [],
+    rightMenuList: [],
+    rightScrollTop: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  cates: [],
+  onLoad() {
+    this.getSmallTypeList()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  getSmallTypeList() {
+    request("bigType/findCategories", {
+      method: 'get'
+    }).then(res => {
+      this.cates = res.message;
+      const rightMenuList = res.message[0].smallTypeList
+      this.setData({
+        baseURL: baseUrl,
+        rightMenuList: rightMenuList,
+        leftMenuList: res.message,
+        selectId: res.message[0].id
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  getRightList(e) {
+    const curItem = e.currentTarget.dataset.item;
+    this.setData({
+      rightMenuList: curItem.smallTypeList || [],
+      selectId: curItem.id,
+      rightScrollTop: 0
+    })
   }
 })
